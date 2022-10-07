dba643035475300c27095341a6f95a2253803d48
不断蜕变灬青春 <wxf18003545940@163.com>
2022-10-07 21:18:29 +0800

    项初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
     <!-- 路由出口 -->
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-fenxiang"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import store from './store'
 // 加载全局样式
 import './styles/index.less'
 
+// 加载Vant全局样式
+import Vant from 'vant'
+
+// 加载全局样式
+import 'vant/lib/index.css'
+
+// 注册使用Vant组件库
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
